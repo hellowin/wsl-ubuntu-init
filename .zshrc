444b5705aaa2db5d0c8f46a56f7c705bf8dff658
Andi N. Dirgantara <andi.n.dirgantara@gmail.com>
2020-05-25 19:40:23 +0700

    Try to add .zshrc

---
+++ .zshrc
@@ -0,0 +1,1 @@
+/home/hellowin/.zshrc
--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install unzip
+sudo apt-get install -y unzip
+sudo apt-get install -y zsh
+cp .zshrc ~/
 wget https://releases.hashicorp.com/terraform/0.12.25/terraform_0.12.25_linux_amd64.zip
 unzip terraform_0.12.25_linux_amd64.zip
 chmod +x terraform
