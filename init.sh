b2e7b3a9408e619602395476c30aaaa605ba990b
Andi N. Dirgantara <andi.n.dirgantara@gmail.com>
2020-05-25 20:31:09 +0700

    Update zsh

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ sudo apt-get install -y unzip
 sudo apt-get install -y zsh
 cp .zshrc ~/
 wget https://releases.hashicorp.com/terraform/0.12.25/terraform_0.12.25_linux_amd64.zip
+NO_INTERACTIVE=true sh -c "$(curl -fsSL https://raw.githubusercontent.com/subtlepseudonym/oh-my-zsh/feature/install-noninteractive/tools/install.sh)"
 unzip terraform_0.12.25_linux_amd64.zip
 chmod +x terraform
 sudo mv terraform /usr/local/bin
