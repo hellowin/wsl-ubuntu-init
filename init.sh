ac7477e64b716b3ecf29edaf85ac51e8d6d8fe3f
Andi N. Dirgantara <andi.n.dirgantara@gmail.com>
2020-06-28 02:28:18 +0700

    remove oh my zsh (#10)

--- init.sh
+++ init.sh
@@ -3,5 +3,3 @@ sudo apt-get upgrade -y
 sudo apt-get install -y build-essential
 sudo apt-get install -y unzip
 sudo apt-get install -y zsh
-cp .zshrc ~/
-NO_INTERACTIVE=true sh -c "$(curl -fsSL https://raw.githubusercontent.com/subtlepseudonym/oh-my-zsh/feature/install-noninteractive/tools/install.sh)"
